265aa3f92908ee43dfec49307a084908668835b3
Nacho <nacho@Nachos-MacBook-Pro.local>
2024-09-26 18:11:23 -0300

    Clear repository

--- src/app/lib/pusher.ts
+++
@@ -1,7 +0,0 @@
-import Pusher from "pusher-js";
-
-const pusherClient = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
-});
-
-export default pusherClient;
